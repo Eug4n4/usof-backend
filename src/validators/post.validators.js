5be74bf401d19ad6943c58fa6cd57a9017c82480
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-09-22 16:05:45 +0300

    ability to provide post status when updating/creating it

--- src/validators/post.validators.js
+++ src/validators/post.validators.js
@@ -5,7 +5,8 @@ import { body } from "express-validator";
 const postValidator = [
     body('title').exists().isLength({ min: 10, max: 50 }),
     body('content').exists().isLength({ min: 50 }),
-    body('categories').isArray().exists()
+    body('categories').isArray({ min: 1 }).exists(),
+    body('active').optional().isInt({ min: 0, max: 1 })
 ]
 
 export { postValidator }
